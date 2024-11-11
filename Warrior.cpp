fe2974314e6a825e29c4f5cbc204e57b034d3390
AnnaSchwartzChas <anna.schwartz@chasacademy.se>
2024-11-11 12:07:29 +0100

    Added files and classes for Player and Warrior

---
+++ Warrior.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Player.h"
+#include "Warrior.h"
+
+Warrior::Warrior(std::string name) : Player::Player()
+{
+    setName(name);
+    setHealth(100);
+    std::string playerClass = "Warrior";
+    setPlayerClass(playerClass);
+}
---
+++ Warrior.h
@@ -0,0 +1,12 @@
+#ifndef WARRIOR_H
+#define WARRIOR_H
+#include <iostream>
+#include <string>
+#include "Player.h"
+
+class Warrior : public Player{
+    public: 
+    Warrior(std::string name);
+};
+#endif
+
