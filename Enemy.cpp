fefd53e62e8138cec057f89670cdd87f5ce059e6
Chengjun723 <yanchengjun521y@gmail.com>
2024-11-11 11:48:31 +0100

    Enemy

---
+++ Enemy.cpp
@@ -0,0 +1,24 @@
+#include "Enemy.h"
+
+using namespace std;
+
+Enemy::Enemy(const string Name, int Health){
+    this->Name = Name;
+    this->Health = 100 ;
+};
+string Enemy::getName() const{
+    return Name;
+};
+int Enemy::getHealth(){
+    return Health;
+};
+void Enemy::setName(const string Name){
+    this->Name = Name;
+};
+void Enemy::setHealth(int Health){
+    if (Health < 0) {
+        throw invalid_argument("Health cannot be negative");
+    }
+    this->Health = Health;
+};
+    
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+#include "Enemy.h"
+using namespace std;
 
 int main()
 {
+    Enemy enemy ("Diablo", 100);
+    cout << "Enemy name: " << enemy.getName() << endl;
+    cout << "Enemy health: " << enemy.getHealth() << endl;
     return 0;
 }
