7d85b0f4de0407773c1fda917bbef6c7835afe5b
Fredrik Storm <fredrik.storm@chasacademy.se>
2024-11-18 14:49:21 +0100

    remove potion destructor

--- src/Potions.cpp
+++ src/Potions.cpp
@@ -3,7 +3,6 @@
 #include "Potions.h"
 
 Potions::Potions(std::string_view name, std::string_view description) : Item(name, description) {}
-Potions::~Potions() {}
 
 int Potions::getHealth() const {
     return this->health;
