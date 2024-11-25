fcdb85a676f92aa5518b266b28f19bc5e6a9abeb
Fredrik Storm <fredrik.storm@chasacademy.se>
2024-11-25 10:08:45 +0100

    inital game loop

--- main.cpp
+++ main.cpp
@@ -1,45 +1,83 @@
-#include <iostream>
-#include <string>
-#include <string_view>
-#include "Enemy.h"
-#include "Warrior.h"
 #include "Game.h"
+#include "GameObject.h"
+#include "Utils.h"
+#include "Warrior.h"
+#include <iostream>
+#include <memory>
+#include <random>
+#include <string>
 
 int main()
 {
-    /*
-    Enemy enemy ("Diablo", 100);
-    cout << "Enemy name: " << enemy.getName() << endl;
-    cout << "Enemy health: " << enemy.getHealth() << endl;
-    //Test av klassen Warrior
-    Warrior player1("Klas", 100, "Warrior");
-    Warrior player2;
-    std:: string name1 = "Knight";
-    std:: string playerClass = "Warrior";
-    int health = 45; 
-    Warrior player3(name1, health, playerClass);
-    cout << "Player name: " << player3.getName() << endl;
-    cout << "Player health: " << player3.getHealth() << endl;
-    cout << "Player class: " << player3.getPlayerClass() << endl;
-    cout <<"Enter the name you want to name your Player of Warrior type: ";
-    string name;
-    getline(cin, name);
-    player2.setName(name);
-    cout << "Player name: " << player2.getName() << endl;
-    cout << "Player health: " << player2.getHealth() << endl;
-    cout << "Player class: " << player2.getPlayerClass() << endl;
-    cout << "Player name: " << player1.getName() << endl;
-    cout << "Player health: " << player1.getHealth() << endl;
-    cout << "Player class: " << player1.getPlayerClass() << endl;
-    player1.setHealth(-10);
-    cout << "Player health: " <<player1.getHealth() <<endl;
-    cout << player1.playerDead() << endl;
-    player1.setHealth(0);
-    cout << "Player health: " <<player1.getHealth() <<endl;
-    cout << player1.playerDead() << endl;
-    */
+    srand(time(0));
 
+    // Create a player object
+    std::shared_ptr<Player> player;
 
+    Utils::clearScreen();
+
+    enum PlayerClass { WARRIOR = 1, MAGE = 2 };
+    std::cout << "1 - Warrior" << std::endl;
+    std::cout << "2 - Mage" << std::endl;
+    std::cout << "Choose your class: ";
+    int choice;
+    std::cin >> choice;
+
+    switch (choice)
+    {
+    case WARRIOR:
+        player = std::make_shared<Warrior>("Klasse Krigare");
+        break;
+
+    case MAGE:
+        player = std::make_shared<Mage>("Karl Trollkarl");
+        break;
+
+    default:
+        break;
+    }
+
+    // Create rooms
+    Room room1("Room 1", "A dark room with a door to the north.");
+    Room room2("Room 2", "Monster Lair");
+    Room room3("Room 3", "Monster Lair2");
+    Room room4("Room 4", "Monster Lair3");
+
+    // Add enemies to the rooms
+    room2.addEnemy(EntityCreator::createRandomEnemy());
+    room3.addEnemy(EntityCreator::createRandomEnemy());
+    room4.addEnemy(EntityCreator::createRandomEnemy());
+
+    // Create a vector of rooms
+    std::vector<Room> rooms = { room1, room2, room3, room4 };
+    size_t currentRoomIndex = 0;
     
+    // Game loop, there's still much to do here
+    while(player->getHealth() > 0) {
+        Utils::clearScreen();
+        Room& currentRoom = rooms[currentRoomIndex];
+        std::cout << "You are in " << currentRoom.getName() << std::endl;
+        std::cout << currentRoom.getDescription() << std::endl;
+
+        // Display enemies in the room
+        currentRoom.displayEnemies();
+
+        // Start combat if there are enemies in the room
+        if(!currentRoom.getEnemies().empty()) {
+            Combat::start(*player, *currentRoom.getEnemies().front());
+        }
+
+        std::cout << "Move to the next room? (y/n): ";
+        std::string move;
+        std::cin >> move;
+        if (move == "y" && currentRoomIndex < rooms.size() - 1) {
+            currentRoomIndex++;
+        } else {
+            break;
+        }
+    }
+
+    std::cout << "You died!" << std::endl;
+
     return 0;
 }
