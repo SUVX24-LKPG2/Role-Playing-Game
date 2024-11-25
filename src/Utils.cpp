c3720cae822578d6859b2301aeca0a4a748c1fcc
Fredrik Storm <fredrik.storm@chasacademy.se>
2024-11-25 09:35:10 +0100

    Fix windows?

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,31 +2,11 @@
 #include "Enemy.h"
 #include <iostream>
 #include <unistd.h>
-
 #ifdef _WIN32
-#include <conio.h>
 #include <windows.h>
-#else
-#include <termios.h>
+#include <conio.h>
 #endif
 
-void Utils::noEcho() {
-    #ifdef _WIN32
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hConsole == INVALID_HANDLE_VALUE) return;
-
-    DWORD mode;
-    if (!GetConsoleMode(hConsole, &mode)) return;
-
-    mode &= ~ENABLE_ECHO_INPUT;
-    SetConsoleMode(hConsole, mode);
-    #else
-    termios term;
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-    #endif
-}
 
 void Utils::clearScreen() {
     #ifdef _WIN32
@@ -46,18 +26,4 @@ void Utils::clearScreen() {
     #else
     std::cout << "\033[2J\033[H" << std::flush;
     #endif
-}
-
-void Utils::setRawMode() {
-    termios term;
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-}
-
-void Utils::resetRawMode() {
-    termios term;
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag |= (ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
