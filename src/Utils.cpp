7a3828922058276878cb028609b59bf23e79e3d9
Fredrik Storm <fredrik.storm@chasacademy.se>
2024-11-25 12:17:42 +0100

    use cls instead

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -4,25 +4,12 @@
 #include <unistd.h>
 #ifdef _WIN32
 #include <windows.h>
-#include <conio.h>
 #endif
 
 
 void Utils::clearScreen() {
     #ifdef _WIN32
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hConsole == INVALID_HANDLE_VALUE) return;
-
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) return;
-
-    DWORD cellCount = csbi.dwSize.X * csbi.dwSize.Y;
-    DWORD count;
-    COORD homeCoords = { 0, 0 };
-
-    FillConsoleOutputCharacter(hConsole, (TCHAR) ' ', cellCount, homeCoords, &count);
-    FillConsoleOutputAttribute(hConsole, csbi.wAttributes, cellCount, homeCoords, &count);
-    SetConsoleCursorPosition(hConsole, homeCoords);
+    system("cls");
     #else
     std::cout << "\033[2J\033[H" << std::flush;
     #endif
